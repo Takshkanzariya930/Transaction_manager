065f8dd33faf41d5d66702979d974f873514a718
Takshkanzariya930 <takshkanzariya930@gmail.com>
2025-07-27 16:34:35 +0530

    completed some more functions

--- Project/test.py
+++ Project/test.py
@@ -4,9 +4,9 @@ conn = mysql.connector.connect(host="localhost", user="root", password='password
 
 cursor = conn.cursor()
 
-def create_user(id,username,email):
+def create_user(username,email):
     try:
-        cursor.execute(f"INSERT INTO users(id,name,email) VALUES ({id},'{username}','{email}')")
+        cursor.execute(f"INSERT INTO users(name,email) VALUES ('{username}','{email}')")
         print("User added...............")
         conn.commit()
         
@@ -15,17 +15,95 @@ def create_user(id,username,email):
         print("User Already exists")
       
       
-def add_member(id,membername,uid):
+def add_member(membername,uid):
     try:
-        cursor.execute(f"INSERT INTO members(mid,name,uid) VALUES ({id},'{membername}',{uid})")
-        print("Memberr added...............")
+        cursor.execute(f"INSERT INTO members(name,uid) VALUES ('{membername}',{uid})")
+        print("Member added...............")
         conn.commit()
         
     except Exception as e:
         print("Error : ", e)
 
-def add_transaction():
-    pass
+def add_transaction(mid,uid,amount,ttype,note,transfered_at):
+    try:
+        cursor.execute(f"SELECT * FROM members WHERE mid = {mid} AND uid = {uid}")
+        result = cursor.fetchone()
+        
+        if result:
+            cursor.execute(f"INSERT INTO trans(mid,uid,amount,ttype,note,transfered_at) VALUES({mid},{uid},{amount},'{ttype}','{note}','{transfered_at}')")
+            print("Transaction recorded...............")
+            conn.commit()
+        
+        else:
+            print("member notfound...........................")
+        
+    except Exception as e:
+        print("Error : ", e)
 
-# create_user(id=1,username="rand",email="rand@gmail.com")
-# add_member(id=1,membername="rand mem",uid=23)
+def delete_transaction(tid,uid):
+    try:        
+        cursor.execute(f"SELECT * FROM trans WHERE tid = {tid} AND uid = {uid}") 
+        result = cursor.fetchone()
+        
+        if result:
+            cursor.execute(f"DELETE FROM trans WHERE tid = {tid}")
+            print("Transaction deleted................. ")
+            conn.commit()
+            
+        else:
+            print("access denied...........................")   
+    except Exception as e:
+        print("Error : ", e)
+
+def total_amount_menber(uid,mid):
+    try:     
+        cursor.execute(f"SELECT SUM(amount) FROM trans WHERE uid={uid} AND mid={mid} AND ttype='lend'")
+        result1 = cursor.fetchone()
+        cursor.execute(f"SELECT SUM(amount) FROM trans WHERE uid={uid} AND mid={mid} AND ttype='borrow'")
+        result2 = cursor.fetchone()
+        print(result1[0] - result2[0])
+        
+    except Exception as e:
+        print("Error : ", e)
+
+def show_all_transaction_member(uid,mid):
+    try:        
+        cursor.execute(f"SELECT * FROM trans WHERE uid={uid} AND mid={mid}")
+        result = cursor.fetchall()
+        
+        for i in result:
+            print(i[3:])
+            
+    except Exception as e:
+        print("Error : ", e)
+        
+def show_all_transaction(uid):
+    try:        
+        cursor.execute(f"SELECT * FROM trans WHERE uid={uid}")
+        result = cursor.fetchall()
+        
+        for i in result:
+            print(i[3:])
+            
+    except Exception as e:
+        print("Error : ", e)
+
+def show_all_members(uid):
+    try:        
+        cursor.execute(f"SELECT * FROM members WHERE uid={uid}")
+        result = cursor.fetchall()
+        
+        for i in result:
+            print(i[:-1])
+            
+    except Exception as e:
+        print("Error : ", e)
+
+# create_user(username="43rand",email="124rand@gmail.com")
+# add_member(membername="rand mem",uid=23)
+# add_transaction(1,23,500,"lend","wafer 23","2024-07-27 14:42:43")
+# delete_transaction(3,23)
+# total_amount_menber(31,23)
+# show_all_transaction_member(31,23)
+# show_all_transaction(31)
+# show_all_members(31)
